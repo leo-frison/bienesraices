87036efe202351079c877478d79140e231393926
Leonardo <leonart_crady205@hotmail.com>
2020-11-15 05:24:20 -0300

    cambio de apiURL

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `https://bienes-raices-posta.herokuapp.com/`,
         queryLimit: 1000,
         contentTypes: ['propiedades', 'paginas', 'categorias']
       }
